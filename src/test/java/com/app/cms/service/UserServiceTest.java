3b3c26ee66da45b6ee4ebf1084359cfcea89891c
Jacek Warchocki <jacek.warchocki@gmail.com>
2020-07-31 21:28:12 +0200

    development

--- src/test/java/com/app/cms/service/UserServiceTest.java
+++ src/test/java/com/app/cms/service/UserServiceTest.java
@@ -1,11 +1,41 @@
 package com.app.cms.service;
 
 
+import com.app.cms.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void shouldCreateUser() {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void shouldDeleteUser() {
+        //given
+
+        //when
+
+        //then
+
+    }
 }
